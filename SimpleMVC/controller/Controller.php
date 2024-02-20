6400a5ea8a3ea118dc801d8922d36dc27bcd73eb
Josue <josuecanul2605@gmail.com>
2024-02-20 11:21:29 -0600

    hot fix

--- SimpleMVC/controller/Controller.php
+++ SimpleMVC/controller/Controller.php
@@ -3,20 +3,22 @@
     require_once(__DIR__ . "/../model/Modelo.php");
     $noticiaModel = new NoticiaModel();
     $pages = $noticiaModel->getPages();
+    
+    if(!isset($_SESSION['page'])) $_SESSION['page'] = 1;
+
     $page = $_SESSION['page'];
+    
     if($_SERVER['REQUEST_METHOD'] == "GET") {
         
-        if(!isset($_SESSION['page'])) $_SESSION['page'] = 1;
+       
 
         if(isset($_GET["next"]) && $_GET['next'] == true) {
-            //$page = $_SESSION['page'];
             $_SESSION['page'] = ($page % $pages) + 1;
             exit($_SESSION['page']);
         }
 
         if(isset($_GET["prev"]) && $_GET['prev'] == true) {
-            //$page = $_SESSION['page'];
-            $_SESSION['page'] = ($page - 2 + $pages) %$pages + 1;
+            $_SESSION['page'] = ($page - 2 + $pages) % $pages + 1;
             exit($_SESSION['page']);
         }
     }
