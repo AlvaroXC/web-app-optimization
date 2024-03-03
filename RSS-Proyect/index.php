15caae508fa49b2f3059a2bbdccc3722b100be15
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2024-03-02 18:11:11 -0600

    prueba

--- RSS-Proyect/index.php
+++ RSS-Proyect/index.php
@@ -6,11 +6,15 @@ $rss_lector = new RSSLector("https://www.xataka.com.mx/feedburner.xml");
 $processed_feed = $rss_lector->process_feed(); //Esto esta mal implementado pero sirve
 $news = $processed_feed['news'];
 include_template('header', true);
+
+debuguear($news);
 ?>
+
+
 
 <?php foreach ($news as $new) : ?>
   <a href="noticia.html" class="contenedor-noticia">
-    <img src="<?php echo $new["img"]?>;" alt="ejemplo" type="image/jpeg">
+    <img src="<?php echo $new["img"]?>" alt="ejemplo" type="image/jpeg">
     <div class="info-noticia">
       <h1 class="titulo-noticia"><?php echo $new["title"];?></h1>
       <p class="categoria"><?php echo $new["categories"]; ?></p>
